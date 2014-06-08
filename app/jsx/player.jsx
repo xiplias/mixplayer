80c9d468b80aa56573bbb899bce8ed7eb9e8ece7
Anders Hansen <xiplias@gmail.com>
2014-06-08 20:49:25 +0200

    Fix progress bar

--- app/jsx/player.jsx
+++ app/jsx/player.jsx
@@ -51,7 +51,7 @@ Progress = React.createClass({
     return Format(Math.round(position/1000) || 0);
   },
   progressCalculate: function (position, duration) {
-    return (position/duration*100)/2 + "%";
+    return (position/duration*100) + "%";
   },
   render: function () {
     var sound = this.props.sound;
